36ae2a8f1c29ce9b44adb52522648c4cda3dc2d7
Tonefro <tfronzeo@gmail.com>
2017-12-13 14:21:34 -0500

    csss markdown updates

--- index.php
+++ index.php
@@ -14,9 +14,6 @@
 	<link rel="stylesheet" href="css/sg-style.css">
 	<!--[if lt IE 9]><link rel="stylesheet" href="css/sg-style-old-ie.css"><![endif]-->
 
-	<!-- https://github.com/sindresorhus/github-markdown-css -->
-	<link rel="stylesheet" href="css/github-markdown.css">
-
 	<!-- Replace below stylesheet with your own stylesheet -->
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="css/spinner.css">
